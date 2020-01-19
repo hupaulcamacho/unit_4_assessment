ba2e71f1c62678e710f957a80eff66b078521c8f
Hupaul <hupaulcamacho@pursuit.org>
2020-01-19 16:13:43 -0500

    add channel name and date uploaded to results

--- my-youtube-web-app/src/Components/SearchResults.jsx
+++ my-youtube-web-app/src/Components/SearchResults.jsx
@@ -11,8 +11,11 @@ const SearchResults = (props) => {
             <div className='video'>
                 <Link to={`/video/${result.id.videoId}`}>
                     <img id={result.id.videoId} src={`${result.snippet.thumbnails.medium.url}`} />
-                </Link>
-                    <p><b>{result.snippet.title}</b></p> 
+                </Link><br/>
+                    <b>{result.snippet.title}</b><br/>
+                    <b><i>{result.snippet.channelTitle}</i></b><br/>
+                    {'Posted: '}<i>{result.snippet.publishedAt}</i> 
+
             </div>
         )
     })
